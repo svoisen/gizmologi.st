41cf1d45dfab4313cc021ee2064abb357c1d9e1e
Sean Voisen <sean@voisen.org>
2012-01-21 19:00:29 -0800

    Update copy, add images and add to cart button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+function getEmail() {
+  return ("<n uers=\"znvygb:fhccbeg@tvmzbybtv.fg\" ery=\"absbyybj\">fhccbeg@tvmzbybtv.fg<\/n>").replace(/[a-zA-Z]/g, 
+  function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+}
 
+$('#footer-email').html(getEmail());
